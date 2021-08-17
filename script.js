3782c5aabf158ef316124b443ab0b653963b28aa
Wafa Mohessin <wafamohessin@gmail.com>
2021-08-17 17:43:59 -0400

    add prompt

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-/*var Numbers = [0,1,2,3,4,5,6,7,8,9]
-var Uppercase =["A","B","C","D","E", "F","G","H","I","J","K","L","M","M","O","P","Q","R","S","T","U","V","W","X,"Y","Z"]
-var Lowercase = ["a","b","c","d","e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x,"y","z"]
-var Symbols = ["!","@","#","$","%","^","&","*",(","),"_","=","+","?","/","|","{","}","[","]","<",">"]
-
-var passwordLength = prompt("Please enter 8 to 128 charactors")
-var numbers = confirm("Enter number or more in yuor password")
-var caps = confirm("Enter at least one Uppercase or more in yuor password") 
-var lower = confirm("Enter lowercase letter or more in yuor password")
-var special = confirm("Enter special character or more in yuor password")*/
-
 // declare array var for chars types
 // prompt user for chars types
 // set the value of valid chars to be equal to all the chars type that the user uses
-// 
-var validChars = ["A","B","C","D","E", "F","G","H","I","J","K","L","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var passwordLength = 128;
-var password = "validChars";
+
+var numbers = [0,1,2,3,4,5,6,7,8,9]
+var uppercase =["A","B","C","D","E","F","G","H","I","J","K","L","M","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lowercase = ["a","b","c","d","e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var symbols = ["!","@","#","$","%","^","&","*",(","),"_","=","+","?","/","|","{","}","[","]","<",">"]
+
+
+var passwordLength = 10;
+var password = "";
+
+
+function generatePassword(){
+
+var passwordLength = prompt("Please enter a password that contains 8-128 characters.");
+var numbers = confirm("Your password should have at least one number.");
+var caps = confirm("Your password should have at least one uppercase."); 
+var lower = confirm("Your password should have at least one lowercase.");
+var special = confirm("Your password should have at least one special character.");
+
+}
 
 // Write password to the #password input
  for (var i = 0; i < passwordLength; i++) {
-   password+= "passwordLength" //?
    
-  //console.log('chars' + validChars[i])
-  function getrandome (chars) {
+   console.log(validChars.length)
+
+    var num = getrandome (validChars.length)
+    console.log(num)
+    console.log(validChars[num])
+    password+= validChars[num] //?
+ 
+   console.log(password)
+   return password
+ }
+
+
+  function getrandome (max) {
     return  Math.floor(Math.random() * max ); 
     
     console.log(Math.random());
   }
-
-   //Math.floor(Math.random() * max ); //chars.lenght);
-   //password.document.getElementById("password").value=password
   
 
 function writePassword() {
@@ -42,8 +54,10 @@ function writePassword() {
   passwordText.value = password;
 }
 
+
 //Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-if (generateBtn){
-console.log(validChars)
+/*if (generateBtn){
+  console.log(validChars.length)
+  
 }
