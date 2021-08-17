11811cb30bd6ac8bc03b4c410ce99507dfc2b36f
Wafa Mohessin <wafamohessin@gmail.com>
2021-08-17 16:16:42 -0400

    add new codes

--- script.js
+++ script.js
@@ -12,28 +12,38 @@ var caps = confirm("Enter at least one Uppercase or more in yuor password")
 var lower = confirm("Enter lowercase letter or more in yuor password")
 var special = confirm("Enter special character or more in yuor password")*/
 
-var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+/?.><,\|{[}]:;"
+// declare array var for chars types
+// prompt user for chars types
+// set the value of valid chars to be equal to all the chars type that the user uses
+// 
+var validChars = ["A","B","C","D","E", "F","G","H","I","J","K","L","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var passwordLength = 128;
-var password = "",
+var password = "validChars";
 
 // Write password to the #password input
  for (var i = 0; i < passwordLength; i++) {
-   var randomNumbers= Math.floor(Math.random() * chars.lenght);
-   password.document.getElementById("password").value=password
+   password+= "passwordLength" //?
+   
+  //console.log('chars' + validChars[i])
+  function getrandome (chars) {
+    return  Math.floor(Math.random() * max ); 
+    
+    console.log(Math.random());
+  }
 
- }
+   //Math.floor(Math.random() * max ); //chars.lenght);
+   //password.document.getElementById("password").value=password
+  
 
-
-/*function writePassword() {
-  var password = generatePassword();
+function writePassword() {
+  password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}*/
+}
 
 //Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-if (){
-  
+if (generateBtn){
+console.log(validChars)
 }
