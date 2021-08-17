04c417ef2595c17ecbab36c925b76eeb686c24f0
Wafa Mohessin <wafamohessin@gmail.com>
2021-08-17 18:03:21 -0400

    I become crazy

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ var generateBtn = document.querySelector("#generate");
 // prompt user for chars types
 // set the value of valid chars to be equal to all the chars type that the user uses
 
-var numbers = [0,1,2,3,4,5,6,7,8,9]
-var uppercase =["A","B","C","D","E","F","G","H","I","J","K","L","M","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var lowercase = ["a","b","c","d","e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var symbols = ["!","@","#","$","%","^","&","*",(","),"_","=","+","?","/","|","{","}","[","]","<",">"]
+//var numbers = [0,1,2,3,4,5,6,7,8,9]
+//var uppercase =["A","B","C","D","E","F","G","H","I","J","K","L","M","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+//var lowercase = ["a","b","c","d","e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+//var symbols = ["!","@","#","$","%","^","&","*",(","),"_","=","+","?","/","|","{","}","[","]","<",">"]
+ 
 
+var validChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","M","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","!","@","#","$","%","^","&","*",(","),"_","=","+","?","/","|","{","}","[","]","<",">", "0","1","2","3","4","5","6","7","8","9"]
 
-var passwordLength = 10;
+var passwordLength = 12;
 var password = "";
 
 
@@ -28,15 +30,15 @@ var special = confirm("Your password should have at least one special character.
 // Write password to the #password input
  for (var i = 0; i < passwordLength; i++) {
    
-   console.log(validChars.length)
+    console.log(validChars.length)
 
     var num = getrandome (validChars.length)
     console.log(num)
     console.log(validChars[num])
-    password+= validChars[num] //?
- 
-   console.log(password)
-   return password
+    
+    //password+= validChars[num]
+    //console.log(password)
+    //return passwordLength
  }
 
 
@@ -60,4 +62,4 @@ generateBtn.addEventListener("click", writePassword);
 /*if (generateBtn){
   console.log(validChars.length)
   
-}
+}*/
