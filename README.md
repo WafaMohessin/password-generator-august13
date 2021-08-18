04f249fd1d5cac3c0bce57d3b357532904089a29
Wafa Mohessin <wafamohessin@gmail.com>
2021-08-18 11:33:18 -0400

    use while loop

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # password-generator-august13
-To generate a password in Javascript we have to do the following. 
-- Make a string consist of lowercase,uppercase, Numbers and Special Characters.
-- prompt the user to follow the instructions.
-- add confirm method to get true and flase value from the user. 
-- use loope to generate a randome password. 
-- use Math.random () method to generate a number in between 0 and 1 then multiply with max.
-- function method so user write a password.
-- finally, add event listener to generate button.
+1. Make a separate array for each type of character " all the caps, all the lowercase, all the numbers, all the characters"
+2. Make a new, empty array called Character_pool
+3. prompt the user to follow the instructions.
+4. add confirm method to get true and flase value from the user. 
+5. use fot loope to generate a randome password. 
+6. use while loop to call the valid characters.
+7. use Math.random () method to generate a number in between 0 and 1 then multiply with max.
+8. function method so user write a password.
+9. finally, add event listener to generate button.
--- script.js
+++ script.js
@@ -5,16 +5,17 @@ var generateBtn = document.querySelector("#generate");
 // prompt user for chars types
 // set the value of valid chars to be equal to all the chars type that the user uses
 
-//var numbers = [0,1,2,3,4,5,6,7,8,9]
-//var uppercase =["A","B","C","D","E","F","G","H","I","J","K","L","M","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-//var lowercase = ["a","b","c","d","e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-//var symbols = ["!","@","#","$","%","^","&","*",(","),"_","=","+","?","/","|","{","}","[","]","<",">"]
+var numbers = [0,1,2,3,4,5,6,7,8,9]
+var uppercase =["A","B","C","D","E","F","G","H","I","J","K","L","M","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lowercase = ["a","b","c","d","e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var symbols = ["!","@","#","$","%","^","&","*",(","),"_","=","+","?","/","|","{","}","[","]","<",">"]
+var Character_pool= []
  
 
 var validChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","M","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","!","@","#","$","%","^","&","*",(","),"_","=","+","?","/","|","{","}","[","]","<",">", "0","1","2","3","4","5","6","7","8","9"]
 
 var passwordLength = 12;
-var password = "";
+var password = "A";
 
 
 function generatePassword(){
@@ -25,7 +26,6 @@ var caps = confirm("Your password should have at least one uppercase.");
 var lower = confirm("Your password should have at least one lowercase.");
 var special = confirm("Your password should have at least one special character.");
 
-}
 
 // Write password to the #password input
  for (var i = 0; i < passwordLength; i++) {
@@ -36,10 +36,19 @@ var special = confirm("Your password should have at least one special character.
     console.log(num)
     console.log(validChars[num])
     
-    //password+= validChars[num]
-    //console.log(password)
-    //return passwordLength
+    password+= validChars[num]
+    console.log(password)
+    return Character_pool
  }
+}
+
+// while loop 
+
+while (Character_pool[Character_pool]){
+  text+= Character_pool[i];i++;
+}
+
+//document.getElementById(password) . innerHTML = text;
 
 
   function getrandome (max) {
@@ -59,7 +68,6 @@ function writePassword() {
 
 //Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-/*if (generateBtn){
-  console.log(validChars.length)
+if (numbers.length){
   
-}*/
+}
